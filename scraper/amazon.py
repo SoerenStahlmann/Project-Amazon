f8a9fa95ebff2371a52cb3933705e577801e77c8
manns <mannstahl@hotmail.de>
2019-08-01 12:46:56 +0200

    Changed the method to retrieve the review site so products with large introduction texts can be scraped. Removed duplicates.

--- scraper/amazon.py
+++ scraper/amazon.py
@@ -16,23 +16,17 @@ import time
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
 
-def get_review_site(url, header):
-    
-    # make request and parse response to a tree     
-    req = requests.get(url, headers = header)
-    tree = html.fromstring(req.content)
-    
-    print(req.content)
-    
-    # extract review link and number of reviews
-    anchor      = tree.xpath(".//a[@data-hook='see-all-reviews-link-foot']")[0]
-    link        = '/'.join(anchor.get("href").split('/')[:4])
-    nbr_reviews = [int(s) for s in anchor.text.split() if s.isdigit()][0]
-    
-    return link, nbr_reviews
+def get_review_site(url):
+
+    url_slugs = url.split('/')
+    url_slugs[4] = 'product-reviews'
+    url_slugs[6] = ''
+    link         = '/'.join(url_slugs)
+
+    return link
 
 
-def to_csv(header, data, fn = "amazon_scrape_csv", **kwargs):
+def to_csv(header, data, fn="amazon_scrape_csv", **kwargs):
     
     # timestamp filename
     fn = fn + "_" + str(int(time.time())) + ".csv"
@@ -50,8 +44,16 @@ def to_csv(header, data, fn = "amazon_scrape_csv", **kwargs):
         
 
 def scrape(url, header=HEADER, **kwargs):
-    BASE_URL        = "https://www.amazon.com"
-    review_url, nbr = get_review_site(url, header)
+
+    # get review url
+    review_url = get_review_site(url)
+
+    # get amount of reviews for pagination
+    base_query = "ref=cm_cr_arp_d_paging_btm_next_{pg}?ie=UTF8&reviewerType=all_reviews&pageNumber={pg}&pageSize=20"
+    page_url   = review_url + base_query.format(pg=1)
+    req        = requests.get(page_url, headers=header)
+    tree       = html.fromstring(req.content)
+    nbr        = int(tree.xpath(".//span[@data-hook='total-review-count']")[0].text)
     
     # create empty list for reviews
     data = []
@@ -59,13 +61,12 @@ def scrape(url, header=HEADER, **kwargs):
     # get amount of pages for product
     pages = math.ceil(nbr/20)
     for page in range(pages):
-        # build link for correct pageination
-        query = "/ref=cm_cr_arp_d_paging_btm_next_{pg}?ie=UTF8&reviewerType=all_reviews&pageNumber={pg}&pageSize=20"\
-                .format(pg = page)
-        page_url = BASE_URL + review_url + query
+        # build link for correct pagination
+        query = base_query.format(pg=page+1)
+        page_url = review_url + query
         
         # make request and parse to tree
-        req  = requests.get(page_url, headers = header)
+        req  = requests.get(page_url, headers=header)
         tree = html.fromstring(req.content)
         
         # get all reviews
--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,8 @@ URL needs to be the product URL  from the Amazon.COM page
 
 # input data
 url    = "https://www.amazon.com/Deep-Learning-Python-Francois-Chollet/dp/1617294438/ref=cm_cr_arp_d_product_top?ie=UTF8"
+url_galaxy = "https://www.amazon.com/TracFone-Samsung-Galaxy-Prepaid-Smartphone/dp/B071RR8NCG/ref=sr_1_2_sspa?keywords=samsung+galaxy+s7&qid=1564654524&s=gateway&sr=8-2-spons&psc=1"
+if __name__ == '__main__':
 
-
-# scrape data
-scraper.amazon.scrape(url)
+    # scrape data
+    scraper.amazon.scrape(url_galaxy)
