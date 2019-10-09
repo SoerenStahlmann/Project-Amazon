2b00e9f979133a99f12f3cf94c7207f7f1bfe045
Soeren <mannstahl@hotmail.de>
2019-10-09 10:46:40 +0200

    Changed method to obtain review links. Added timestamp

--- scraper/amazon.py
+++ scraper/amazon.py
@@ -10,6 +10,7 @@ from lxml import html
 import math
 import csv
 import time
+from datetime import datetime as dt
 
 
 # set header global
@@ -17,17 +18,21 @@ HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 
 
 def get_review_site(url, header):
-    
+
+    # get review url
+    link       = url.split('/')
+    link[4]    = "product-reviews"
+    link       = link[:6]
+    link       = '/'.join(link)
+    review_url = link + "/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
+
     # make request and parse response to a tree     
-    req = requests.get(url, headers = header)
+    req  = requests.get(review_url, headers=header)
     tree = html.fromstring(req.content)
     
-    print(req.content)
-    
     # extract review link and number of reviews
-    anchor      = tree.xpath(".//a[@data-hook='see-all-reviews-link-foot']")[0]
-    link        = '/'.join(anchor.get("href").split('/')[:4])
-    nbr_reviews = [int(s) for s in anchor.text.split() if s.isdigit()][0]
+    review_cnt  = tree.xpath(".//span[@data-hook='total-review-count']")[0].text
+    nbr_reviews = [int(s) for s in review_cnt.split() if s.isdigit()][0]
     
     return link, nbr_reviews
 
@@ -50,9 +55,8 @@ def to_csv(header, data, fn = "amazon_scrape_csv", **kwargs):
         
 
 def scrape(url, header=HEADER, **kwargs):
-    BASE_URL        = "https://www.amazon.com"
     review_url, nbr = get_review_site(url, header)
-    
+
     # create empty list for reviews
     data = []
     
@@ -61,11 +65,11 @@ def scrape(url, header=HEADER, **kwargs):
     for page in range(pages):
         # build link for correct pageination
         query = "/ref=cm_cr_arp_d_paging_btm_next_{pg}?ie=UTF8&reviewerType=all_reviews&pageNumber={pg}&pageSize=20"\
-                .format(pg = page)
-        page_url = BASE_URL + review_url + query
+                .format(pg=page)
+        page_url = review_url + query
         
         # make request and parse to tree
-        req  = requests.get(page_url, headers = header)
+        req  = requests.get(page_url, headers=header)
         tree = html.fromstring(req.content)
         
         # get all reviews
@@ -73,10 +77,16 @@ def scrape(url, header=HEADER, **kwargs):
         for review in reviews:
             
             # get title and content of review
-            title = review.xpath(".//a[@data-hook='review-title']/span")[0].text
-            body  = review.xpath(".//span[@data-hook='review-body']/span")[0].text
-            note  = ""
-            
+            title     = review.xpath(".//a[@data-hook='review-title']/span")[0].text
+            body      = review.xpath(".//span[@data-hook='review-body']/span")[0].text
+            note      = ""
+            rating    = review.xpath(".//i[@data-hook='review-star-rating']/span")[0].text
+
+            # TODO: TIMEZONE??
+            # timestamp
+            timestamp = review.xpath(".//span[@data-hook='review-date']")[0].text
+            timestamp = time.mktime(dt.strptime(timestamp, "%B %d, %Y").timetuple())
+
             # check if user has a verified purchase or was payed
             vine  = review.xpath(".//a[@data-hook='linkless-format-strip-whats-this']")
             verified = review.xpath(".//span[@data-hook='avp-badge']")
@@ -86,8 +96,8 @@ def scrape(url, header=HEADER, **kwargs):
             elif vine:
                 note = vine[0].text
             
-            data.append((title, body, note))
-            
+            data.append((title, body, note, rating, timestamp))
+
     # write data to csv file
-    columns = ['title', 'content', 'note']
+    columns = ['title', 'content', 'note', 'rating', 'timestamp']
     to_csv(columns, data)
